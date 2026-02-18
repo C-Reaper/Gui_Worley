e89970416680ca1c4489ed8adff2f4aabe9ef5e6
codeleaded <gnu.c.reaper@gmail.com>
2026-02-18 19:10:51 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -15,13 +15,35 @@
 
 
 Worley wy;
+Vec2* p;
 
 void Setup(AlxWindow* w){
 	RGA_Set(Time_Nano());
-	wy = Worley_New(10.0f,10.0f,10);
+	wy = Worley_New(100.0f,100.0f,4);
+	p = NULL;
 }
 void Update(AlxWindow* w){
 	TransformedView_HandlePanZoom(&wy.tv,w->Strokes,GetMouse());
+	const Vec2 m = TransformedView_ScreenWorldPos(&wy.tv,GetMouse());
+
+	if(Stroke(ALX_MOUSE_L).PRESSED){
+		for(int i = 0;i<wy.points.size;i++){
+			Vec2* pp = (Vec2*)Vector_Get(&wy.points,i);
+			if(Circle_Point((Circle[]){ Circle_New(*pp,1.0f) },m)){
+				p = pp;
+			}
+		}
+	}
+	if(Stroke(ALX_MOUSE_L).DOWN){
+		if(p){
+			*p = m;
+		}
+	}
+	if(Stroke(ALX_MOUSE_L).RELEASED){
+		p = NULL;
+	}
+
+	Worley_Update(&wy);
 
 	Clear(BLACK);
 
