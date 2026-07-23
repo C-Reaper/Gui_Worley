b8331db44537e62c120ae289ea899581d5bcdd38
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:35:55 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,11 +1,11 @@
 #if defined __linux__
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/Worley.h"
 #elif defined _WINE
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/Worley.h"
 #elif defined _WIN32
-#include "F:/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "F:/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "F:/home/codeleaded/System/Static/Library/Worley.h"
 #elif defined(__APPLE__)
 #error "Apple not supported!"
